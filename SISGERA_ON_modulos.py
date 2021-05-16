d6d9bc20a81126b40a312bde9490981b51b0da98
Danilo Santos <artededan@protonmail.ch>
2021-05-16 11:47:10 -0300

    (SISGERA ON) Aprimoramento do algoritmo + inserção de dados via SQLite

--- SISGERA_ON_modulos.py
+++ SISGERA_ON_modulos.py
@@ -5,8 +5,14 @@
 # Maintainer: Danilo Santos (danilosantos07@protonmail.com)
 # versão 20210513 (Codename: Taste)
 
+import sqlite3
 import time
 from datetime import datetime
+
+# Script de conexão do banco de dados
+banco = sqlite3.connect('sisgera_on.db')
+cursor = banco.cursor()
+
 
 def menu():
     # Menu de opções do sistema
@@ -36,41 +42,66 @@ def menu():
 
 
 def cadastrar_anuncio():
-    dados = dict()
     while True:
-        # Inserir nome do anúncio
-        dados['nome_anuncio'] = str(input("Nome do anúncio: "))
+        # 1) Inserir nome do anúncio
+        nome_anuncio = str(input("Nome do anúncio: "))
 
-        # Inserir nome do cliente
-        dados['cliente'] = str(input("Nome do cliente: "))
+        # 2) Inserir nome do cliente
+        cliente = str(input("Nome do cliente: "))
 
-        # Obter data de início
-        dados['obter_data_inicio'] = input("Data de início [DD/MM/YYYY]: ")
-        dados['data_inicio'] = datetime.strptime(dados['obter_data_inicio'], '%d/%m/%Y')
+        # 3) Obter data de início do contrato
+        obter_data_inicio = input("Data de início [DD/MM/YYYY]: ")
+        # 3.1) Uso da função strptime para formatação da string para data:
+        converter_data_inicio = datetime.strptime(obter_data_inicio, '%d/%m/%Y')
+        # 3.2) SQLite não aceita manipulação do módulo datetime com a função strptime para inserção do dado no banco, logo
+        # módulo tem que convertido para strftime:
+        data_inicio = converter_data_inicio.strftime('%d/%m/%Y')
 
-        # Obter data de término
-        dados['obter_data_termino'] = input("Data de término [DD/MM/YYYY]: ")
-        dados['data_termino'] = datetime.strptime(dados['obter_data_termino'], '%d/%m/%Y')
+        # 4) Obter data de término do contrato
+        obter_data_termino = input("Data de término [DD/MM/YYYY]: ")
+        converter_data_termino = datetime.strptime(obter_data_termino, '%d/%m/%Y')
+        data_termino = converter_data_termino.strftime('%d/%m/%Y')
 
-        # Investimento/dia
-        dados['investimento_dia'] = float(input("Quanto o cliente deseja investir por dia? (R$) "))
+        # 5) Investimento/dia do cliente
+        investimento_dia = float(input("Quanto o cliente deseja investir por dia? (R$) "))
 
         # Formula responsável por calcular a diferença de dias inserida no cadastro
         # de anúncios para obter o valor total investido pelo cliente
-        quantidade_dias = (dados['data_termino'] - dados['data_inicio']).days
+        quantidade_dias = (converter_data_termino - converter_data_inicio).days
         dias_hora = quantidade_dias * 24
-        valor_total_investido = dias_hora * dados['investimento_dia']
+        valor_total_investido = dias_hora * investimento_dia
+        
+        # Cálculo da quantidade máxima de visualizações
+        valor_anuncio = 1.00 / 30
+        alcance_compartilhamento = 3 * 40 * 4
+        maxima_visualizacoes = valor_total_investido * valor_anuncio * alcance_compartilhamento
 
+        # Cálculo da quantidade máxima de cliques
+        maxima_cliques = valor_total_investido / 12 * 20
+
+        # Cálculo da quantidade máxima de compartilhamentos
+        maxima_compartilhamentos = valor_total_investido / 3 * 4
+        
+        # Inserção de dados do cadastro (comentar linhas 86 e 87 caso precisar testar a saída de dados)
+        #cursor.execute("INSERT INTO sisgera VALUES('"+nome_anuncio+"','"+cliente+"','"+data_inicio+"','"+data_termino+"','"+investimento_dia+"')")
+        #banco.commit()        
+        
+        # Área de testes [Output] - Comentar linhas 90 a 106 para evitar mostrar dados desnecessários no sistema de produção
         print()
         print('-=' * 30)
-        print("Nome do anúncio: ", dados['nome_anuncio'])
-        print("Cliente: ", dados['cliente'])
-        print("Data de início do contrato: ", dados['data_inicio'])
-        print("Data de término do contrato: ", dados['data_termino'])
+        print("AREA DE TESTES [OUTPUT - SEM BANCO DE DADOS]")
+        print()
+        print("Nome do anúncio: ", nome_anuncio)
+        print("Cliente: ", cliente)
+        print("Data de início do contrato: ", data_inicio)
+        print("Data de término do contrato: ", data_termino)
         print("Dias de contrato: ", quantidade_dias)
-        print("Dias/Hora de contrato: ", dias_hora, 'H')
-        print(f'Investimento do cliente por dia: (R$) ', dados['investimento_dia'])
+        print("Dias/Hora de contrato: ", dias_hora,'H')
+        print(f'Investimento do cliente por dia: (R$) {investimento_dia:,.2f}')
         print(f'Valor total investido é de R$ {valor_total_investido:,.2f}')
+        print("Quantidade máxima de visualizações: ", int(maxima_visualizacoes))
+        print("Quantidade máxima de cliques: ", int(maxima_cliques))
+        print("Quantidade máxima de compartilhamentos: ", int(maxima_compartilhamentos))
         print('-=' * 30)
         print()
         resposta = str(input('Deseja cadastrar outro cliente? [S/N] '))
